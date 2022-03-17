ae522949da758bc821014fac7981edf8a3aa434d
Rohakartoha <93739049+Rohakartoha@users.noreply.github.com>
2022-03-17 20:25:22 +0200

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -3,9 +3,9 @@
 
 pragma solidity ^0.8.0;
 
-import "contracts/IERC20.sol";
-import "contracts/IERC20Metadata.sol";
-import "contracts/Context.sol";
+import "IERC20.sol";
+import "IERC20Metadata.sol";
+import "Context.sol";
 
 /**
  * @dev Implementation of the {IERC20} interface.
